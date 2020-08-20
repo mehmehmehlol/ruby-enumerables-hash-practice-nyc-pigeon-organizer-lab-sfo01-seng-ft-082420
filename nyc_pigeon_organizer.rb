037106ed69f8a5d05b5167cf3ffb91264a5d8ddd
Megan Lo <hkmeganlo@gmail.com>
2020-08-20 22:48:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,8 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         binding.pry
-        final_array << name
-        
+        if !final_array[name]
+          final_array[name] = {}
       end
     end
     final_array
