89fb6f04bd911f4dbe0e8aeefe0f86ce2d64de0d
Megan Lo <hkmeganlo@gmail.com>
2020-08-20 22:50:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,6 +9,9 @@ def nyc_pigeon_organizer(data)
         binding.pry
         if !final_array[name]
           final_array[name] = {}
+        end
+        if !final_array[name][key]
+          
       end
     end
     final_array
